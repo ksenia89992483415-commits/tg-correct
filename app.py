981312a2f649142b2b4fe9081ae52d81a21458e2
ksenia89992483415-commits <ksenia89992483415@gmail.com>
2025-07-30 12:23:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import os
 import telebot
 
 TOKEN = os.getenv('5771183937:AAHBYjt7maRu20ZuypuBCtAsGBUzrrrilV8')
-TARGET_CHAT_ID = int(os.getenv('5771183937'))
+TARGET_CHAT_ID = int(os.getenv('-5771183937'))
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda message: True)
 def forward_message(message):
     try:
-        bot.forward_message(5771183937, message.chat.id, message.message_id)
+        bot.forward_message(-5771183937, message.chat.id, message.message_id)
     except Exception as e:
         print(f"Ошибка при пересылке: {e}")
 
